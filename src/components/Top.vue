3013a708748c3ae562403b2467c182314037b123
Mariana <mxpixel@yahoo.com.br>
2020-07-28 21:25:47 -0300

    Top (Header) e pagina de Inicio

---
+++ src/components/Top.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">APEQ-ES</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Sobre</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contato</a>
+            </li>
+          </ul>
+          <button class="btn btn-info rounded-pill my-2 my-sm-0" type="submit">Associe-se</button>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Top"
+};
+</script>
+
---
+++ src/components/templates/Inicio.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container  text-center mt-4">
+    <h1 class="display-4"><p>Associação de profissionais</p><p>  da Engenharia Química do Espírito Santo </p></h1>
+
+    <br>
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="https://picsum.photos/1024/480/?image=10" class="d-block w-100" />
+        </div>
+        <div class="carousel-item">
+          <img src="https://picsum.photos/1024/480/?image=12" class="d-block w-100" />
+        </div>
+        <div class="carousel-item">
+          <img src="https://picsum.photos/1024/480/?image=22" class="d-block w-100" />
+        </div> 
+      </div>
+      <a
+        class="carousel-control-prev"
+        href="#carouselExampleIndicators"
+        role="button"
+        data-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a
+        class="carousel-control-next"
+        href="#carouselExampleIndicators"
+        role="button"
+        data-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Inicio"
+};
+</script>
