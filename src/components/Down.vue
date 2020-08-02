ca15847ab4b18ba45dd37c3c7291939c1a4c621d
Mariana <mxpixel@yahoo.com.br>
2020-08-02 14:14:03 -0300

    Alteracao no header e no footer

--- src/components/Down.vue
+++ src/components/Down.vue
@@ -1,53 +1,75 @@
 <template>
-  <div  class="navbar-fixed-bottom pt-5">
-    <nav class="navbar navbar-expand navbar-light bg-light">
-      <div class="container pt-5">
-        <a class="navbar-brand" href="#">APEQ-ES</a>
-          <ul class="nav navbar-nav navbar-center">
-            <li class="nav-item">
-              <a class="nav-link" href="#"><router-link  class="footer-color" to="home">Inicio</router-link></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"><router-link  class="footer-color" to="sobre">Sobre</router-link></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"><router-link  class="footer-color" to="contato">Contato</router-link></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"><router-link class="footer-color" to="associese">Associe-se</router-link></a>
-            </li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.facebook.com/apeq.es">
-                <font-awesome-icon :icon="['fab', 'facebook-f']" />
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.linkedin.com/in/apeq-es">
-                <font-awesome-icon :icon="['fab', 'linkedin-in']" />
-              </a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="https://www.instagram.com/apeq.es/">
-              <font-awesome-icon :icon="['fab', 'instagram']" />
-            </a>
-            </li>
-          </ul>
-            
+    <div class="container-fluid-fixed-bottom bg-light pt-4 mt-5">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-2 col-sm-6">
+            <blockquote class="blockquote text-center"><p class="ml-0">APEQ-ES</p></blockquote>
+          </div>
+          <div class="col-md-8 col-sm-9">
+            <ul class="list-inline text-center">
+              <li class="list-inline-item pr-3">
+                <a href="#">
+                  <router-link class="footer-color" to="home">Inicio</router-link>
+                </a>
+              </li>
+
+              <li class="list-inline-item pr-3">
+                <a href="#">
+                  <router-link class="footer-color" to="sobre">Sobre</router-link>
+                </a>
+              </li>
+
+              <li class="list-inline-item pr-3">
+                <a class href="#">
+                  <router-link class="footer-color" to="contato">Contato</router-link>
+                </a>
+              </li>
+
+              <li class="list-inline-item ">
+                <a class href="#">
+                  <router-link class="footer-color" to="associese">Associe-se</router-link>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-2 col-sm-6">
+            <ul class="list-inline text-center">
+              <li class="list-inline-item pr-3">
+                <a class="footer-color" href="https://www.facebook.com/apeq.es">
+                  <font-awesome-icon :icon="['fab', 'facebook-f']" />
+                </a>
+              </li>
+              <li class="list-inline-item pr-3">
+                <a class="footer-color" href="https://www.linkedin.com/in/apeq-es">
+                  <font-awesome-icon :icon="['fab', 'linkedin-in']" />
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="footer-color" href="https://www.instagram.com/apeq.es/">
+                  <font-awesome-icon :icon="['fab', 'instagram']" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </nav>
-  </div>
+      <div class="footer-copyright text-center py-3 mb-0 pb-0">
+        © 2020 Copyright:
+        <a href="#" class="footer-color">APEQ-ES</a>
+      </div>
+    </div>
+
 </template>
 
 <script>
 export default {
-  name: 'Down'
+  name: "Down",
 };
 </script>
 
 <style scoped>
-.footer-color{
+.footer-color {
   color: gray;
 }
+
 </style>
--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,8 +1,41 @@
 <template>
   <div>
+    <!--
+    <div class="container-fluid bg-light">
+      <div class="container">
+      <div class="row">
+        <div class="col-9">
+          <ul class="list-inline mr-auto pt-2 mb-1">
+            <li class="list-inline-item pr-4">
+              <a class="top-style" href="https://www.facebook.com/apeq.es">
+                <font-awesome-icon :icon="['fab', 'facebook-f']" />
+              </a>
+            </li>
+            <li class="list-inline-item pr-4">
+              <a class="top-style" href="https://www.linkedin.com/in/apeq-es">
+                <font-awesome-icon :icon="['fab', 'linkedin-in']" />
+              </a>
+            </li>
+            <li class="list-inline-item ">
+              <a class="top-style" href="https://www.linkedin.com/in/apeq-es">
+                <font-awesome-icon :icon="['fab', 'instagram']" />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-3 text-left pt-2 mb-1 pl-5">
+            <a class="top-style" href="mailto: sapeq@email.com">apeq@gmail.com</a>
+        </div>
+      </div>
+      </div>
+    </div>
+    -->
+
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-        <a class="navbar-brand" href="#"><router-link class="top-style-title" to="home">APEQ-ES</router-link></a>
+        <a class="navbar-brand" href="#">
+          <router-link class="top-style-title" to="home">APEQ-ES</router-link>
+        </a>
         <button
           class="navbar-toggler"
           type="button"
@@ -18,16 +51,24 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-              <a class="nav-link" href="#"><router-link class="top-style" to="home">Inicio</router-link></a>
+              <a class="nav-link" href="#">
+                <router-link class="top-style" to="home">Inicio</router-link>
+              </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#"><router-link class="top-style" to="sobre">Sobre</router-link></a>
+              <a class="nav-link" href="#">
+                <router-link class="top-style" to="sobre">Sobre</router-link>
+              </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#"><router-link class="top-style" to="contato">Contato</router-link></a>
+              <a class="nav-link" href="#">
+                <router-link class="top-style" to="contato">Contato</router-link>
+              </a>
             </li>
           </ul>
-          <button class="btn btn-info rounded-pill my-2 my-sm-0" type="submit"><router-link class="btn-style" to="associese" >Associe-se</router-link></button>
+          <button class="btn btn-info rounded-pill my-2 my-sm-0" type="submit">
+            <router-link class="btn-style" to="associese">Associe-se</router-link>
+          </button>
         </div>
       </div>
     </nav>
@@ -36,18 +77,18 @@
 
 <script>
 export default {
-  name: 'Top'
-}
+  name: "Top",
+};
 </script>
 
 <style scoped>
-.top-style-title{
+.top-style-title {
   color: black;
 }
-.top-style{
+.top-style {
   color: gray;
 }
-.btn-style{
+.btn-style {
   color: white;
 }
 </style>
