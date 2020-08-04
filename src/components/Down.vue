13b0f5a59245e646c8cd8fc5f86b0e249456adee
Mariana <mxpixel@yahoo.com.br>
2020-08-03 21:48:44 -0300

    Botao do associado responsivo

--- src/components/Down.vue
+++ src/components/Down.vue
@@ -1,64 +1,67 @@
 <template>
-    <div class="container-fluid-fixed-bottom bg-light pt-4 mt-5">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-md-2 col-sm-6">
-            <blockquote class="blockquote text-center"><p class="ml-0">APEQ-ES</p></blockquote>
-          </div>
-          <div class="col-md-8 col-sm-9">
-            <ul class="list-inline text-center">
-              <li class="list-inline-item pr-3">
-                <a href="#">
-                  <router-link class="footer-color" to="home">Inicio</router-link>
-                </a>
-              </li>
+  <div class="container-fluid bg-light pt-4 mt-5">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-2 col-sm-6">
+          <blockquote class="blockquote text-center">
+            <p class="ml-0">APEQ-ES</p>
+          </blockquote>
+        </div>
 
-              <li class="list-inline-item pr-3">
-                <a href="#">
-                  <router-link class="footer-color" to="sobre">Sobre</router-link>
-                </a>
-              </li>
+        <div class="col-md-8 col-sm-9">
+          <ul class="list-inline text-center">
+            <li class="list-inline-item pr-3">
+              <a href="#">
+                <router-link class="footer-color" to="home">Inicio</router-link>
+              </a>
+            </li>
 
-              <li class="list-inline-item pr-3">
-                <a class href="#">
-                  <router-link class="footer-color" to="contato">Contato</router-link>
-                </a>
-              </li>
+            <li class="list-inline-item pr-3">
+              <a href="#">
+                <router-link class="footer-color" to="sobre">Sobre</router-link>
+              </a>
+            </li>
 
-              <li class="list-inline-item ">
-                <a class href="#">
-                  <router-link class="footer-color" to="associese">Associe-se</router-link>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div class="col-md-2 col-sm-6">
-            <ul class="list-inline text-center">
-              <li class="list-inline-item pr-3">
-                <a class="footer-color" href="https://www.facebook.com/apeq.es">
-                  <font-awesome-icon :icon="['fab', 'facebook-f']" />
-                </a>
-              </li>
-              <li class="list-inline-item pr-3">
-                <a class="footer-color" href="https://www.linkedin.com/in/apeq-es">
-                  <font-awesome-icon :icon="['fab', 'linkedin-in']" />
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a class="footer-color" href="https://www.instagram.com/apeq.es/">
-                  <font-awesome-icon :icon="['fab', 'instagram']" />
-                </a>
-              </li>
-            </ul>
-          </div>
+            <li class="list-inline-item pr-3">
+              <a class href="#">
+                <router-link class="footer-color" to="contato">Contato</router-link>
+              </a>
+            </li>
+
+            <li class="list-inline-item">
+              <a class href="#">
+                <router-link class="footer-color" to="associese">Associe-se</router-link>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="col-md-2 col-sm-6">
+          <ul class="list-inline text-center">
+            <li class="list-inline-item pr-3">
+              <a class="footer-color" href="https://www.facebook.com/apeq.es">
+                <font-awesome-icon :icon="['fab', 'facebook-f']" />
+              </a>
+            </li>
+            <li class="list-inline-item pr-3">
+              <a class="footer-color" href="https://www.linkedin.com/in/apeq-es">
+                <font-awesome-icon :icon="['fab', 'linkedin-in']" />
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="footer-color" href="https://www.instagram.com/apeq.es/">
+                <font-awesome-icon :icon="['fab', 'instagram']" />
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
-      <div class="footer-copyright text-center py-3 mb-0 pb-0">
-        © 2020 Copyright:
-        <a href="#" class="footer-color">APEQ-ES</a>
-      </div>
     </div>
-
+    <div class="text-center py-3 mb-0 pb-0">
+      © 2020 Copyright:
+      <a href="#" class="footer-color">APEQ-ES</a>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -72,4 +75,7 @@ export default {
   color: gray;
 }
 
+.padb {
+  padding-bottom: 100%;
+}
 </style>
